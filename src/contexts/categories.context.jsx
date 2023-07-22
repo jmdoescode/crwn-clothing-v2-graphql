a3fc5011cce0e3515e130d124921fc04ec4d4122
jm codes <jmdoescode@gmail.com>
2023-07-21 17:13:43 -0700

    234. Updating Components To Consume GraphQL Values

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -24,10 +24,23 @@ export const CategoriesProvider = ({ children }) => {
 	const { loading, error, data } = useQuery(COLLECTIONS); //pass graphQL query into useQuery
 	const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log('loading', loading);
-  console.log('graphQL data', data);
+	useEffect(() => {
+		if (data) {
+			const { collections } = data;
+			const collectionsMap = collections.reduce((acc, collection) => {
+				const { title, items } = collection;
+				acc[title.toLowerCase()] = items;
+				return acc;
+			}, {});
 
-	const value = { categoriesMap };
+      setCategoriesMap(collectionsMap);
+		}
+	}, [data]);
+
+	console.log("loading", loading);
+	console.log("graphQL data", data);
+
+	const value = { categoriesMap, loading };
 	return (
 		<CategoriesContext.Provider value={value}>
 			{children}
